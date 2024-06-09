99ce08fddf6312ea88c0f37b00101bbe71244edf
achmadky <achmad.dhikrillah@gmail.com>
2024-06-10 01:10:06 +0700

    add assert pytest ui

--- test_ui/test_eten_ui.py
+++ test_ui/test_eten_ui.py
@@ -5,5 +5,5 @@ from selenium import webdriver
 def test_home_page_load():
     driver = webdriver.Chrome()  # You can use other browsers too
     driver.get("https://eten-ui.vercel.app/")
-    # assert "Eten" in driver.title
+    assert "Food" in driver.title
     driver.quit()
